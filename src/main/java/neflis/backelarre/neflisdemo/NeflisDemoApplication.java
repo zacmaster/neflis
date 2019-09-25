0a4ca063ae9dbf4d6fbf3e48bde0b4abc00bf944
Romina <rominaglzj@gmail.com>
2019-09-25 20:17:04 -0300

    Nuevo proyecto de neflis

---
+++ src/main/java/neflis/backelarre/neflisdemo/NeflisDemoApplication.java
@@ -0,0 +1,13 @@
+package neflis.backelarre.neflisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeflisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeflisDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neflis/backelarre/neflisdemo/NeflisDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package neflis.backelarre.neflisdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeflisDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
